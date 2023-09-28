33b96f08177593badac86b2d9e570d2d48579473
ctx-devansh <devansh.the.photofreak@gmail.com>
2023-09-28 02:19:28 -0500

    V1 - Added buttons and functionaltiy

---
+++ function.js
@@ -0,0 +1,10 @@
+let ROCK = 'rock';
+let PAPER = 'paper';
+let SCISSORS = 'scissors';
+let ROCK_PATH = '/images/rock128.png'
+let PAPER_PATH = '/images/paper128.png'
+let SCISSORS_PATH = '/images/scissors128.png'
+
+function changeImage(a){
+    document.getElementById(playerChoice).src = a
+}
