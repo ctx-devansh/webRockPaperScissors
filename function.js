6b0c64e6cd0c5eae2c29a4c5efb69101a4aba6ff
ctx-devansh <devansh.the.photofreak@gmail.com>
2023-09-28 02:28:45 -0500

    Changed File paths

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
 let ROCK = 'rock';
 let PAPER = 'paper';
 let SCISSORS = 'scissors';
-let ROCK_PATH = '/images/rock128.png'
-let PAPER_PATH = '/images/paper128.png'
-let SCISSORS_PATH = '/images/scissors128.png'
+let ROCK_PATH = 'https://ibb.co/h1rqfqR'
+let PAPER_PATH = 'https://ibb.co/ryccV41'
+let SCISSORS_PATH = 'https://ibb.co/JzZt4hq'
 
 function changeImage(a){
     document.getElementById(playerChoice).src = a
